b4e561e8b0ff133125f90735de972338b67f72e7
Under-the-stars <127274036+Under-the-stars@users.noreply.github.com>
2023-12-25 20:57:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ from langchain.schema import SystemMessage
 
 import gru_inference
 
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")  # ""#
+# with st.sidebar:
+#     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")  # ""#
     #os.environ["OPENAI_API_KEY"] = openai_api_key
 llm = ChatOpenAI(model_name="gpt-3.5-turbo")
 greeting_msg = "Hello! How Can I help you ? Do you want Predict stocks today?"
